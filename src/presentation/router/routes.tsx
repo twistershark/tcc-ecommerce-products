5dfbe56631babf0e0ff7668c94962c65ead79a4f
Paulo Victor <paulovmel@gmail.com>
2023-11-16 21:21:26 -0300

    chore: updates import in routes configuration

--- src/presentation/router/routes.tsx
+++ src/presentation/router/routes.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { Home } from "../pages/home";
+import Home from "../pages/home";
 
 import "tailwindcss/tailwind.css";
-import { Products } from "../pages/products";
+import Products from "../pages/products";
 
 export default function ProductsListRoutes() {
   return (
